6b37b7f5c1fceec8fa3c6a5b0ec3f7bd8c6a0095
Ahmed Shan <ahmedshan16@gmail.com>
2022-12-12 17:16:34 +0500

    Fixed setwebhook

--- setWebhook.php
+++ setWebhook.php
@@ -5,6 +5,9 @@ use Heliumframework\Telegram;
 include __DIR__ . '/bootstrap.php';
 
 $telegram = new Telegram($appConfig->bot_username, $appConfig->bot_token);
-$response = $telegram->setWebhook($config->base_url."/hook.php");
-echo '<pre>'; print_r($response); echo '</pre>';
+$url = $appConfig->base_url."/hook.php";
+$response = $telegram->setWebhook($url);
+
+echo "Setting webook to -> $url\n";
+print_r($response);
 exit;
